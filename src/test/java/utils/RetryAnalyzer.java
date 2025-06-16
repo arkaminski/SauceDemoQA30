25c21f42aa4c52d48e45e24cb967db01e47922c8
arkaminski <arturkaminski0612@gmail.com>
2025-06-16 18:20:56 +0200

    add gh-pages

--- src/test/java/utils/RetryAnalyzer.java
+++
@@ -1,9 +0,0 @@
-//package tests;
-
-//public class RetryAnalyzer {
-   // public class RetryAnalyzer implements IAnnotationTransformer {
-      //  @Override
-       // public void transform(ITestAnnotation annotation, Class testClass, Constructor testConstructor, Method testMethod) {
-         //   annotation.setRetryAnalyzer(tests.Retry.class);
-       // }
-   // }
